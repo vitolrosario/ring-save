6fbdf81547f707ab8537278bb36d25690eef4c1c
Victor Rosario <vrosario@simetricaconsulting.com>
2023-10-24 12:03:57 -0400

    puppeteer --no-sandbox for sudo

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ class App {
       authStrategy: new LocalAuth(),
       // proxyAuthentication: { username: 'username', password: 'password' },
       puppeteer: { 
-          // args: ['--proxy-server=proxy-server-that-requires-authentication.example.com'],
+          args: ['--no-sandbox'],
           headless: true
       }
     });
